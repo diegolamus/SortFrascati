aa0db020fff0f08d58e4cb5b25ef42e9092caccc
julianfelipe27 <juligury@gmail.com>
2018-10-11 12:16:24 -0500

    ShellSort

--- Sort/server/src/main/java/ServerShell.java
+++ Sort/server/src/main/java/ServerShell.java
@@ -9,7 +9,20 @@ public class ServerShell implements Sort
 
     public final String[] sort(final String[] strings)
     {
-      //TODO
+        int salto, aux, i;
+        boolean cambios;
+        for(salto=A.length/2; salto!=0; salto/=2){
+            cambios=true;
+            while(cambios){ // Mientras se intercambie algún elemento
+              cambios=false;
+              for(i=salto; i< A.length; i++) // se da una pasada
+                if(A[i-salto].compareTo(A[i])>0){ // y si están desordenados
+                    aux=A[i]; // se reordenan
+                    A[i]=A[i-salto];
+                    A[i-salto]=aux;
+                    cambios=true; // y se marca como cambio.
+                }
+            }
+        }
     }
-
 }
